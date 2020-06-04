7bd08828923ff1c4898990c41a2c209eea06220e
alifele <ali.fele@gmail.com>
2020-06-04 19:48:27 +0430

    fitz added

---
+++ step by step/software/main.py
@@ -0,0 +1,60 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import *
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QKeySequence as QKSec
+from Ribbon.RibbonButton import RibbonButton
+from Ribbon.Icons import get_icon
+from Ribbon.RibbonTextbox import RibbonTextbox
+from Ribbon.RibbonWidget import *
+import qdarkstyle
+from PyQt5.QtGui import QIcon
+import ribbon_actions
+import qdarkstyle.style_rc
+import sys
+
+from Tabs.Dynamical_systems.neuron_models.Huxly.Implementation.gui import huxly_dialog
+from Tabs.Dynamical_systems.neuron_models.Fithz.Implementation.gui import fitz_dialog
+
+
+from appear import UI_elemts
+
+
+
+
+
+class mainwindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = UI_elemts()
+        self.ui.Setup_UI(self)
+
+        self.init_signals()
+
+
+
+
+
+    def init_signals(self):
+        self.ui.dynamic_tab.huxly_model.triggered.connect(self.huxly_clicked)
+        self.ui.dynamic_tab.Fithz.triggered.connect(self.fitz_clicked)
+
+
+
+    def huxly_clicked(self):
+        self.huxly_window = huxly_dialog(self.ui)
+
+    def fitz_clicked(self):
+        self.fitz_window = fitz_dialog(self.ui)
+
+
+
+
+
+def start():
+    app = QApplication([])
+    main = mainwindow()
+    #main.test()
+    main.show()
+    sys.exit(app.exec_())
+
+start()
