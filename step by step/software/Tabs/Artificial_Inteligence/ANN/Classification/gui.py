05475c957df8147ad9900c9fe4715c0fcf32f798
alifele <ali.fele@gmail.com>
2020-06-06 00:07:07 +0430

    functionality to ANN tab added

--- step by step/software/Tabs/Artificial_Inteligence/ANN/Classification/gui.py
+++ step by step/software/Tabs/Artificial_Inteligence/ANN/Classification/gui.py
@@ -3,6 +3,9 @@ from Tabs.Artificial_Inteligence.ANN.Classification.main import Ui_Dialog
 import numpy as np
 import matplotlib.pyplot as plt
 from PyQt5 import QtGui
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class classification(QtWidgets.QDialog):
@@ -16,12 +19,20 @@ class classification(QtWidgets.QDialog):
         self.ui.spinBox.valueChanged.connect(self.layers_chaged)
         self.ui.spinBox.setValue(int(4))
         self.ui.pushButton_2.clicked.connect(self.start_train)
+        self.ui.pushButton.clicked.connect(self.predict)
         self.ui.push_clear.clicked.connect(self.clear_canv)
+
+    def predict(self):
+        self.ui.label.pixmap().save('log/handwritten.png')
+        image = imageio.imread('log/handwritten.png')
+        plt.imshow(image)
+        plt.show()
+
 
     def clear_canv(self):
         self.ui.label.pixmap().fill(QtGui.QColor('black'))
         self.ui.label.update()
-        
+
     def layers_chaged(self, val):
         self.ui.tableWidget.setRowCount(int(val))
         for i in range(val):
@@ -34,7 +45,6 @@ class classification(QtWidgets.QDialog):
         for i in range(int(self.ui.spinBox.text())):
             number_of_neurons_list.append(int(self.ui.tableWidget.item(i,0).text()))
 
-        print(number_of_neurons_list)
 
 
 
